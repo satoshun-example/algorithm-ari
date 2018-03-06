4f179d18ce5f68c88c1a40427775beec5724b29a
satoshun <shun.sato1@gmail.com>
2018-03-06 23:27:23 +0000

    maze with queue

--- python/2-1-4_test.py
+++ python/2-1-4_test.py
@@ -35,7 +35,6 @@ def _puzzles(sx, sy, l):
                     n.append((xx, yy))
     return c
 
-
 def test_puzzle():
     for (n, m, puzzle, result) in [
         (10, 10,
@@ -54,3 +53,45 @@ def test_puzzle():
         22),
     ]:
         assert puzzles(n, m, puzzle) is result
+
+
+## 迷路 with queue
+def puzzles2(n, m, l):
+    sx, sy = _find_start(m, m, l)
+    l[sx][sy] = 0
+    mx = len(l)
+    my = len(l[0])
+    q = [(sx, sy)]
+    for (x, y) in q:
+        for xd, yd in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            xx = x + xd
+            yy = y + yd
+            if xx < 0 or xx >= mx:
+                continue
+            if yy < 0 or yy >= my:
+                continue
+            if l[xx][yy] == 'G':
+                return l[x][y] + 1
+            if l[xx][yy] == '.':
+                l[xx][yy] = l[x][y] + 1
+                q.append((xx, yy))
+    return False
+
+def test_puzzle2():
+    for (n, m, puzzle, result) in [
+        (10, 10,
+            [
+                ['#', 'S', '#', '#', '#', '#', '#', '#', '.', '#'],
+                ['.', '.', '.', '.', '.', '.', '#', '.', '.', '#'],
+                ['.', '#', '.', '#', '#', '.', '#', '#', '.', '#'],
+                ['.', '#', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['#', '#', '.', '#', '#', '.', '#', '#', '#', '#'],
+                ['.', '.', '.', '.', '#', '.', '.', '.', '.', '#'],
+                ['.', '#', '#', '#', '#', '#', '#', '#', '.', '#'],
+                ['.', '.', '.', '.', '#', '.', '.', '.', '.', '.'],
+                ['.', '#', '#', '#', '#', '.', '#', '#', '#', '.'],
+                ['.', '.', '.', '.', '#', '.', '.', '.', 'G', '#'],
+            ],
+        22),
+    ]:
+        assert puzzles2(n, m, puzzle) is result
