2c60f1fde4b347f6d76a8b9bf6b7efa6210e729b
satoshun <shun.sato1@gmail.com>
2018-03-11 23:08:50 +0000

    section donyoku

--- python/2-2-2_test.py
+++ python/2-2-2_test.py
@@ -1,5 +1,28 @@
 #!/usr/local/bin/python3
 
+## section/区間 解答
+def section2(n, s, t):
+    count = 0
+    current = 0
+    while True:
+        next_pos = None
+        for i in range(n):
+            if s[i] >= current:
+                if next_pos is None or next_pos > t[i]:
+                    next_pos = t[i]
+        if next_pos is None:
+            break
+        current = next_pos
+        count += 1
+    return count
+
+def test_section2():
+    for (n, s, t, result) in [
+        (5, [1,2,4,6,8], [3,5,7,9,10], 3),
+    ]:
+        assert section2(n, s, t) is result
+
+
 ## section/区間
 def section(n, s, t):
     costs = [c - st for (st, c) in zip(s, t)]
